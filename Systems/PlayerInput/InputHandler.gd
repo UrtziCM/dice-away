279f257ed40dc52224a748837a5875520c114b16
Yxini <urcrmedev@gmail.com>
2024-11-28 16:29:13 +0100

    update: updated InputHandler, when space is pressed dice are thrown.

--- Systems/PlayerInput/InputHandler.gd
+++ Systems/PlayerInput/InputHandler.gd
@@ -1,6 +1,10 @@
 extends Node
 
+@onready
+var MainGameHandler = get_parent()
+
 var last_input_event: InputEvent
 
-func _input(event):
-	last_input_event = event
+func _process(delta):
+	if Input.is_action_just_released("ui_accept"):
+		MainGameHandler.throw_dice()
