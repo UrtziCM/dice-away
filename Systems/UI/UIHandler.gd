6aa46caf70b91efb142beeaf079bf4bd9372efaf
Yxini <urcrmedev@gmail.com>
2024-11-29 22:44:38 +0100

    update: updated UIHandler to use progress bar and enemy sprite
    added both UI elements to UIElements dictionary
    
    ProgressBar now has a flash method, update method and setup method:
    
    - _flash_health: Flashes health red for the given time.
    
    - setup_target_score: sets progress value to max
    
    - update_current_score: updates the progress bar to show the score fraction given as parameter.
    
    Enemy has a flash method for better player feedback:
    
    - _flash_enemy: Flashes enemy red for the given time.

--- Systems/UI/UIHandler.gd
+++ Systems/UI/UIHandler.gd
@@ -5,5 +5,49 @@ var UILayer = get_node("CanvasLayer")
 
 @onready
 var UIElements: Dictionary = {
+	"TargetScoreHealthBar": UILayer.get_node("TargetScoreHealthBar"),
+	"EnemyTexture": UILayer.get_node("EnemyTextureRect"),
 }
+
+func _process(delta):
+	pass
+
+func _flash_health(flash_duration: int):
+	var should_flash = true
+	var start_time = Time.get_unix_time_from_system()
+	while (should_flash):
+		if UIElements["TargetScoreHealthBar"].tint_progress != Color.RED:
+			UIElements["TargetScoreHealthBar"].tint_progress = Color.RED
+		else:
+			UIElements["TargetScoreHealthBar"].tint_progress = Color.GREEN
+		if start_time + flash_duration < Time.get_unix_time_from_system():
+			should_flash = false
+		await get_tree().create_timer(0.25).timeout
+	UIElements["TargetScoreHealthBar"].tint_progress = Color.GREEN
+
+func _flash_enemy(flash_duration: int):
+	var should_flash = true
+	var start_time = Time.get_unix_time_from_system()
+	while (should_flash):
+		if UIElements["EnemyTexture"].modulate == Color.WHITE:
+			UIElements["EnemyTexture"].modulate = Color.RED
+		else:
+			UIElements["EnemyTexture"].modulate = Color.WHITE
+		if start_time + flash_duration < Time.get_unix_time_from_system():
+			should_flash = false
+		await get_tree().create_timer(0.25).timeout
+	UIElements["EnemyTexture"].modulate = Color.WHITE
+
+func setup_target_score(max_score: int, score: int):
+	UIElements["TargetScoreHealthBar"].value = score / max_score
+
+func update_current_score(max_score: float, score: float):
+	if score > 0:
+		var health_tween = create_tween()
+		health_tween.set_ease(Tween.EASE_OUT)
+		health_tween.set_trans(Tween.TRANS_CIRC)
+		health_tween.tween_property(UIElements["TargetScoreHealthBar"], "value", score / max_score ,0.75)
+		_flash_health(0.75)
+		_flash_enemy(0.75)
+
 
